db1503e16b9a61e7e5ef1eceb87eff98d3fe4265
bsmiquelini <bruno.miquelini@msn.com>
2025-03-20 17:01:03 -0300

    Ajuste função de deploy

--- .github/actions/deployAwsS3/main.js
+++ .github/actions/deployAwsS3/main.js
@@ -2,17 +2,15 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-function getInputs() {
+function Deploy() {
+  // Coleta parametros de entrada
   const bucketName = core.getInput('bucket-name');
   const distPath = core.getInput('dist-path');
   const region = core.getInput('region');
-}
-
-function deployToS3(bucketName, distPath, region) {
+  
   // Deploy to S3
   exec.exec(`echo aws s3 sync ${distPath} s3://${bucketName} --recursive --region ${region}`);
   core.notice('Deploy do path ' + distPath + ' para o bucket ' + bucketName + ' na região ' + region);
 }
 
-getInputs();
-deployToS3(bucketName, distPath, region);
+Deploy();
