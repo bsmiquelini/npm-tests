7c763707cfb45f40688a007cbe2becdb02ae3b6f
bsmiquelini <bruno.miquelini@msn.com>
2025-03-21 11:23:59 -0300

    Update entrypoint.sh for AWS S3 deployment: remove redundant environment variable assignments

--- .github/actions/deployAwsS3-docker/entrypoint.sh
+++ .github/actions/deployAwsS3-docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-AWS_ACCESS_KEY_ID="$1"
-AWS_SECRET_ACCESS_KEY="$2"    
-AWS_BUCKET_NAME="$3"
-AWS_REGION="$4"
-AWS_DIST_PATH="$5"
-
 echo "Deploying to AWS S3"
 echo "aws s3 sync ${AWS_DIST_PATH} s3://${AWS_BUCKET_NAME} --recursive --region ${AWS_REGION}"
 siteUrl="https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com"  
