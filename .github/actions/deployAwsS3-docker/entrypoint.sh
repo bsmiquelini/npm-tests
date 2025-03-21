c55dbf8fe29eba8ecc25fcc79a1fc16048bacfe6
bsmiquelini <bruno.miquelini@msn.com>
2025-03-21 11:26:10 -0300

    Update entrypoint.sh for AWS S3 deployment: add comment for script language clarification

--- .github/actions/deployAwsS3-docker/entrypoint.sh
+++ .github/actions/deployAwsS3-docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Aqui é um script, mas poderia ser qualquer linguagem que queira trabalhar, nesse caso precisaria apenas ajustar o Dockerfile e a aplicação
 echo "Deploying to AWS S3"
 echo "aws s3 sync ${AWS_DIST_PATH} s3://${AWS_BUCKET_NAME} --recursive --region ${AWS_REGION}"
 siteUrl="https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com"  
